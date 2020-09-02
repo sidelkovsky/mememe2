ffd75ff50d61b09bd54bfe2fa4a583220cd2511c
Sidelkovsky <sidelkovsky@tuta.io>
2020-09-02 15:26:12 +0200

    Editing text fields: show keyboard "on focus" and hide "on return key"

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -22,34 +22,37 @@ class ViewController: UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-         
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         updateUI(isPortrait: UIDevice.current.orientation.isPortrait)
+        subscribeToKeyboardNotifications()
     }
-
-    // small font for landscape of all devices
-    // small font for iPhone SE; ios 13 is not supported on iPhone < 6, so we don't care others
-    // normal font for everybody else
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        unsubscribeFromKeyboardNotifications()
+    }
+    
+    // MARK: UI Related Methods
+    
     func updateUI(isPortrait: Bool) {
-
+        
         var fontSize = 36
         
         let smallScreen = Device.size() < Size.screen4_7Inch
-                    
+        
         // iPhone SE in Landscape mode
         if smallScreen && !isPortrait {
             topTextFieldTopConstraint.constant = 9
             bottomTextFieldBottomConstraint.constant = -9
             fontSize = 22
-        // iPhone SE in Portrait mode and other models in Landscape Mode
+            // iPhone SE in Portrait mode and other models in Landscape Mode
         } else if smallScreen || !isPortrait {
             topTextFieldTopConstraint.constant = 18
             bottomTextFieldBottomConstraint.constant = -18
             fontSize = 28
-        // iPhone 6 and higher in Portrait mode
+            // iPhone 6 and higher in Portrait mode
         } else {
             topTextFieldTopConstraint.constant = 36
             bottomTextFieldBottomConstraint.constant = -36
@@ -76,6 +79,52 @@ class ViewController: UIViewController, UITextFieldDelegate {
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         updateUI(isPortrait: UIDevice.current.orientation.isPortrait)
     }
-
+    
+    // MARK: Keyboard related methods
+    
+    func subscribeToKeyboardNotifications() {
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+    }
+    
+    @objc func keyboardWillShow(_ notification:Notification) {
+        
+        if bottomTextField.isFirstResponder {
+            
+            view.frame.origin.y -= getKeyboardHeight(notification)
+            
+        }
+        
+    }
+    
+    @objc func keyboardWillHide(_ notification:Notification) {
+        
+        view.frame.origin.y = 0
+        
+    }
+    
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+        
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.cgRectValue.height
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        return true
+        
+    }
+    
 }
-
