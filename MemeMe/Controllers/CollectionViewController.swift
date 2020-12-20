4d019150bac700ca18a892fff8abe07f747dfcea
Sidelkovsky <sidelkovsky@tuta.io>
2020-12-21 00:55:26 +0300

    Collectionve view to display images

--- MemeMe/Controllers/CollectionViewController.swift
+++ MemeMe/Controllers/CollectionViewController.swift
@@ -17,4 +17,21 @@ class CollectionViewController: UICollectionViewController {
         
     }
     
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return MemeDataStore.shared.getMemes().count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MemeCollectionCell",for: indexPath) as! MemeCollectionCell
+        
+        let item = MemeDataStore.shared.getMemes()[indexPath.row]
+
+        cell.imageView?.image = item.memedImage
+
+        return cell
+        
+        
+    }
+    
 }
---
+++ MemeMe/Views/MemeCollectionCell.swift
@@ -0,0 +1,15 @@
+//
+//  MemeCollectionCell.swift
+//  MemeMe
+//
+//  Created by Kirill Sidelkovsky on 09.11.2020.
+//  Copyright © 2020 sidelkovsky.com. All rights reserved.
+//
+
+import UIKit
+
+class MemeCollectionCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
